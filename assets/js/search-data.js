d41ce91554bcddef90aa605f0ee73034f043f55f
ShuohanWang <shuohanwang2001@gmail.com>
2025-12-01 02:43:02 +0000

    Deploying to gh-pages from @ ShuohanWang/shuohanwang.github.io@09c15242ebca1c51b195adac93dd892c3a6e5237 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -497,6 +497,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-academia_edu',
+        title: 'Academia_edu',
+        section: 'Socials',
+        handler: () => {
+          window.open("", "_blank");
+        },
+      },{
         id: 'social-cv',
         title: 'CV',
         section: 'Socials',
@@ -508,7 +515,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%53%68%75%6F%68%61%6E.%57%61%6E%67@%6A%61%63%6B%73.%73%64%73%74%61%74%65.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/shuohanwang", "_blank");
         },
       },{
         id: 'social-inspire',
